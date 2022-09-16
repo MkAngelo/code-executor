be59a2d1ba652236c3e57ad4735cb3accefae5d5
mkangelo <mikequi000@gmail.com>
2022-09-16 16:24:17 -0500

    Getting ans

--- onlineide/views.py
+++ onlineide/views.py
@@ -1,9 +1,32 @@
-from sys import settrace
-from tkinter import E
+
 from django.shortcuts import render
+import subprocess
+from os import remove
 
 def home(request):
     if request.method == "POST":
         #import pdb; pdb.set_trace()
-        print(request.POST)
+        lang = request.POST['languages']
+        source = request.POST['code']
+        name = "code." + lang
+        route = "static/temp/"+name
+        temp = open(route, "w")
+        temp.write(source)
+        temp.close()
+
+        ans=''
+        if(lang == 'py'): ans=subprocess.run(["python3", route], capture_output=True, text=True)
+        # elif(lang == 'c'): subprocess.run(["gcc",route])
+        # elif(lang == 'cpp'): subprocess.run([])
+        # elif(lang == 'java'): subprocess.run([])
+        
+        if ans.stderr:
+            # print("Stderr:", ans.stderr)
+            ans = "Upps... something was wrong"
+        else:
+            ans = ans.stdout
+        
+        context = {'ans': ans}  
+        remove(route)
+        return render(request, "index.html", context)
     return render(request, "index.html")
